a53710d7951b7cd5bddf87189bb44f24080f6678
bazragod1-cyber <bazragod1@gmail.com>
2026-03-22 00:56:48 -0400

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import TelegramBot from "node-telegram-bot-api";
-
-const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
-
-bot.on("message", (msg) => {
-  bot.sendMessage(msg.chat.id, "👑 Miserbot v4 is alive, Your Highness.");
-});
-
-console.log("Miserbot v4 is running...");
